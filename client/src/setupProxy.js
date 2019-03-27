b0d66f029de45f02851c1d496975c1e26572be08
tzetzo <tzvetanmarinov@yahoo.com>
2019-03-27 09:39:20 +0200

    no message

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,6 +1,6 @@
 //https://stackoverflow.com/questions/52605997/when-specified-proxy-in-package-json-must-be-a-string
-// const proxy = require("http-proxy-middleware");
-// module.exports = app => {
-//   app.use(proxy("/auth/google", { target: "http://localhost:5000/" }));
-//   app.use(proxy("/api/*", { target: "http://localhost:5000/" }));
-// };
+const proxy = require("http-proxy-middleware");
+module.exports = app => {
+  app.use(proxy("/auth/google", { target: "http://localhost:5000/" }));
+  app.use(proxy("/api/*", { target: "http://localhost:5000/" }));
+};
