e09639e982bff6d3a596209095b2a84035223676
tzetzo <tzvetanmarinov@yahoo.com>
2020-03-28 12:14:28 +0200

    Facebook ID & Secret added for prod

--- config/prod.js
+++ config/prod.js
@@ -1,6 +1,8 @@
 module.exports = {
   googleClientID: process.env.GOOGLE_CLIENT_ID,
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  FACEBOOK_APP_ID: process.env.FACEBOOK_APP_ID,
+  FACEBOOK_APP_SECRET: process.env.FACEBOOK_APP_SECRET,
   mongoURI: process.env.MONGO_URI,
   cookieKey: process.env.COOKIE_KEY,
   stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
