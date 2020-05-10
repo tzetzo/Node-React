5505687d060e1656785c1e126bb6b7e0a493f304
tzetzo <tzvetanmarinov@yahoo.com>
2020-05-10 12:26:17 +0300

    clean up

--- config/keys.js
+++ config/keys.js
@@ -2,9 +2,8 @@ if (process.env.NODE_ENV === "production") {
   //auto set by Heroku to "production"
   module.exports = require("./prod");
 } else if (process.env.NODE_ENV === "ci") {
-  console.log("Travis-CI test from else if: ", process.env.NODE_ENV);
+  //for our Travis-CI environment
   module.exports = require("./ci");
 } else {
-  console.log("Travis-CI test from else: ", process.env.NODE_ENV);
   module.exports = require("./dev");
 }
