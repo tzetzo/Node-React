360421f5de53d89ab74d726be0f4a396d66c9b1e
tzetzo <tzvetanmarinov@yahoo.com>
2020-05-10 09:37:53 +0300

    env var added for redis server on prod

--- config/prod.js
+++ config/prod.js
@@ -8,5 +8,6 @@ module.exports = {
   stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
   stripeSecretKey: process.env.STRIPE_SECRET_KEY,
   sendGridKey: process.env.SEND_GRID_KEY,
-  redirectDomain: process.env.REDIRECT_DOMAIN
+  redirectDomain: process.env.REDIRECT_DOMAIN,
+  redisURL: process.env.REDIS_URL,
 };
