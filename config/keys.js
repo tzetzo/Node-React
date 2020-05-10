d21bb4a50f12e3fca1284bea7c91bc2df34b7170
tzetzo <tzvetanmarinov@yahoo.com>
2020-05-10 11:56:28 +0300

    added console.log for travis-ci debug

--- config/keys.js
+++ config/keys.js
@@ -2,7 +2,9 @@ if (process.env.NODE_ENV === "production") {
   //auto set by Heroku to "production"
   module.exports = require("./prod");
 } else if (process.env.NODE_ENV === "ci") {
-  module.exports = require("./ci.js");
+  console.log("Travis-CI test from else if: ", process.env.NODE_ENV);
+  module.exports = require("./ci");
 } else {
-  module.exports = require("./dev.js");
+  console.log("Travis-CI test from else: ", process.env.NODE_ENV);
+  module.exports = require("./dev");
 }
